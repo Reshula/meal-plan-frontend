584787e0534c1c87babdec99b2e32c0a30e5430c
Reshula <reyhana328@gmail.com>
2024-08-21 21:05:45 -0400

    react-icons were changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+
+import React,  { useEffect, useState } from 'react';
+
 import './App.css';
+import { MyMeals } from './MyMeals';
+import { getAllMeals, addMeal, editMeal, deleteMeal } from './FetchMeals';
+
 
 function App() {
+
+const [ mymeal, setMyMeal ] = useState([]);
+const [ title, setTitle ] = useState("");
+const [ editing, setEditing] = useState(false);
+const [ mealId, setMealId] = useState("")
+
+
+useEffect(() =>{
+  getAllMeals(setMyMeal)
+},[])
+
+const updatingInput = (_id, title) =>{
+  setEditing(true)
+  setTitle(title)
+  setMealId(_id)
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Meal Plan</h1>
+      <input
+      type="text"
+      placeholder="Add a meal"
+      value={title}
+      onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <button 
+      disabled={!title}
+      onClick=
+      {editing ? () => editMeal (mealId, title, setMyMeal, setTitle, setEditing) :() => addMeal(title, setTitle, setMyMeal )}>
+      {editing ? "Edit" : "Add"}
+      </button>
+      {mymeal.map((meal) => <MyMeals text={meal.title} key={meal._id}
+      updatingInput={() => updatingInput(meal._id, meal.title)}
+      deleteMeal={() => deleteMeal(meal._id, setMyMeal)}
+      />
+    
+    )}
+
+     
+      
     </div>
   );
 }
